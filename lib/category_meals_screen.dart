f006a2e09558a2dfe4c0599cfa0a6ba51b69e355
mahin1995 <mohimanul8@gmail.com>
2021-08-24 22:30:54 +0600

    from home pc

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Text("The Meal screan")
+    );
+  }
+}
